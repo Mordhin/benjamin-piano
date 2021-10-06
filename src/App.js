318f6d252a56c9c32a0d03a2994616a738b9a31c
Julien Corion <astram59@gmail.com>
2021-10-06 16:40:50 +0200

    links

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Paragraphe } from "./components/Paragraphe.js";
 import benji from "./assets/benji avatar.jpg";
 import piano from "./assets/benji clavier.jpg";
 import { Button } from "./components/Button.js";
+import {CgChevronRightR} from "react-icons/cg";
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
       <Navbar />
       <Banner />
       <div className="container mx-auto mt-20 flex flex-col items-center">
-        <Paragraphe title="Qui suis-je ?" image={benji} className="mb-10">
+        <Paragraphe title="Qui suis-je ?" image={benji} className="mb-5">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
           incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
           exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
@@ -19,8 +20,8 @@ const App = () => {
           Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim 
           id est laborum.
         </Paragraphe>
-        <Button size="60">Mes vidéos</Button>
-        <Paragraphe title="Ce que je propose" image={piano} right className="mt-20 mb-10">
+        <div className="w-full cursor-pointer flex justify-end items-center text-primary-400 hover:text-primary-100">Mes vidéos <CgChevronRightR className="ml-2"/></div>
+        <Paragraphe title="Ce que je propose" image={piano} right className="mt-20 mb-5">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
           incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
           exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
@@ -28,10 +29,8 @@ const App = () => {
           Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim 
           id est laborum.
         </Paragraphe>
-        <div className="w-full flex justify-around">
-          <Button size="60">Mes tarifs</Button>
-          <Button size="60" primary>Je réserve un cours</Button>
-        </div>
+        <div className="w-full cursor-pointer flex justify-end items-center text-primary-400 hover:text-primary-100">Mes tarifs <CgChevronRightR className="ml-2"/></div>
+        <Button className="mt-10 mb-20" size="60" primary>Je réserve un cours</Button>
       </div>
     </>
   );
