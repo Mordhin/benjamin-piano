8d7762f83bde283ca5e162dae6f707a5587305b6
Julien Corion <astram59@gmail.com>
2021-10-08 18:11:49 +0200

    navbar responsive & hamburger

--- src/components/Navlink.js
+++ src/components/Navlink.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {Link} from "react-scroll";
 
-export const Navlink = ({to, children}) => {
+export const Navlink = ({to, className, children}) => {
   return (
-    <Link to={to} className="mx-3 cursor-pointer transition-all duration-300 w-20 text-center" activeClass="font-bold" smooth={true} duration={1000} spy={true} offset={-100}>
+    <Link to={to} className={`cursor-pointer transition-all duration-300 ${className}`} activeClass="font-bold" smooth={true} duration={1000} spy={true} offset={-100}>
       {children}
     </Link>
   )
