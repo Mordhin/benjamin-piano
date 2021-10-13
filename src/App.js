22fe4811d5c1868ff51cb1f9464c2154102004e7
Julien Corion <astram59@gmail.com>
2021-10-13 17:40:01 +0200

    pricing done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import piano from "./assets/benji clavier.jpg";
 import { Button } from "./components/Button.js";
 import {CgChevronRightR} from "react-icons/cg";
 import { scrollSpy } from "react-scroll";
+import { PricingCard } from "./components/PricingCard.js";
 
 const App = () => {
   useEffect(() => {
@@ -38,7 +39,14 @@ const App = () => {
         <div className="w-full cursor-pointer flex justify-end items-center text-primary-400 hover:text-primary-100">Mes tarifs <CgChevronRightR className="ml-2"/></div>
         <Button className="mt-10 mb-20" size="60" primary>Je réserve un cours</Button>
       </div>
-      <div id="pricing"></div>
+      <div id="pricing" className="container mx-auto flex flex-col items-center">
+        <PricingCard title="Cours d'une heure" price1="25€" price2="20€" className="mb-20"/>
+        <PricingCard title="Cours d'une demie-heure" price1="15€" price2="13€" className="mb-10"/>
+        <div className="w-full">Forfait de 10 cours d'une heure : 200€</div>
+        <div className="w-full">Forfait de 10 cours d'une demie-heure : 130€</div>
+        <Button className="mt-10 mb-20" size="60" primary>Je réserve un cours</Button>
+      </div>
+      <div className="mb-20"></div>
     </>
   );
 }
